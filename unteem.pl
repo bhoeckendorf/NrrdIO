420cd48813865578e5d2bee60e653ad03fa777b2
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2005-01-10 03:39:38 +0000

    added hack to include #include of namespace mangler

--- unteem.pl
+++ unteem.pl
@@ -1,8 +1,15 @@
 #
 # This helps in converting teem source files into NrrdIO source files,
 # by changing the way #includes are done, and by excluding the lines
-# delimited by "BEGIN non-NrrdIO" and "END non-NrrdIO"
+# delimited by "BEGIN non-NrrdIO" and "END non-NrrdIO", as well as doing
+# some other hacks
 #
+
+if (exists $ENV{"ITK_NRRDIO"}) {
+    $ITK = 1;
+} else {
+    $ITK = 0;
+}
 
 $printing = 1;
 while (<>) {
@@ -11,7 +18,9 @@ while (<>) {
     s|#include "biff.h"|#include "NrrdIO.h"|g;
     s|#include "nrrd.h"|#include "NrrdIO.h"|g;
     s|#include <teem(.*)>|#include "teem$1"|g;
-    s|\/\* NrrdIO-hack-000 \*\/|#define TEEM_BUILD 1|g;
+    if ($ITK) {
+	s|\/\* NrrdIO-hack-000 \*\/|\/\* THE FOLLOWING INCLUDE IS ONLY FOR THE ITK DISTRIBUTION.\n   This header mangles the symbols in the NrrdIO library, preventing\n   conflicts in applications linked against two versions of NrrdIO. \*\/\n#include "itk_NrrdIO_mangle.h"|g;
+    }
     s|.* \/\* NrrdIO-hack-001 \*\/|#if 1|g;
     print if $printing;
     $printing = 1 if (m/END non-NrrdIO/);
