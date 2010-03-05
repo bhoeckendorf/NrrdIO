17151af635bfcc5d7fa0c2fcb25e17d42702de9d
kindlmann <kindlmann@3d70eeeb-363e-0410-a505-8a46323a89f2>
2010-03-05 22:31:24 +0000

    backing off unnecessary changes

--- teemEndian.h
+++ teemEndian.h
@@ -1,25 +1,23 @@
 /*
-  NrrdIO: stand-alone code for basic nrrd functionality
+  Teem: Tools to process and visualize scientific data and images              
   Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
   Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
- 
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any
-  damages arising from the use of this software.
- 
-  Permission is granted to anyone to use this software for any
-  purpose, including commercial applications, and to alter it and
-  redistribute it freely, subject to the following restrictions:
- 
-  1. The origin of this software must not be misrepresented; you must
-     not claim that you wrote the original software. If you use this
-     software in a product, an acknowledgment in the product
-     documentation would be appreciated but is not required.
- 
-  2. Altered source versions must be plainly marked as such, and must
-     not be misrepresented as being the original software.
- 
-  3. This notice may not be removed or altered from any source distribution.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  (LGPL) as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also
+  include exceptions to the LGPL that facilitate static linking.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 
@@ -30,28 +28,11 @@
 */
 
 #ifndef TEEM_ENDIAN
-
-/* what byte order */
-/* All compilers that support Mac OS X define either __BIG_ENDIAN__ or
-__LITTLE_ENDIAN__ to match the endianness of the architecture being
-compiled for. This is not necessarily the same as the architecture of
-the machine doing the building. In order to support Universal Binaries on
-Mac OS X, we prefer those defines to decide the endianness.
-On other platform, we use the result of the TRY_RUN. */
-#if !defined(__APPLE__)
-#if @CMAKE_WORDS_BIGENDIAN@
-#define TEEM_ENDIAN 4321
-#else
-#define TEEM_ENDIAN 1234
-#endif
-#else
-#if defined(__BIG_ENDIAN__)
-#define TEEM_ENDIAN 4321
-#else
-#define TEEM_ENDIAN 1234
-#endif
-#endif
-
+#  error TEEM_ENDIAN not defined, see architecture-specific .mk file or check compilation options
+#elif TEEM_ENDIAN == 1234
+#  /* okay, its little endian */
+#elif TEEM_ENDIAN == 4321
+#  /* okay, its big endian */
 #else
 #  error TEEM_ENDIAN not set to 1234 (little endian) or 4321 (big endian), see architecture-specific .mk file or check compilation options
 #endif
